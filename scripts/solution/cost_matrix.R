de590be5e4edf2523e1f885410fa46b65e4bcb6c
Laurigit <irualgm@gmail.com>
2020-01-28 16:41:23 +0200

    more efficent formulation

--- scripts/solution/cost_matrix.R
+++ scripts/solution/cost_matrix.R
@@ -1,6 +1,27 @@
 
 
 #aggr_to_slots <- aggr_to_slots[1:30]
+cycler_id <- 1
+optaimal_moves_to_finish <- function(cycler_id, game_status, deck_status)
+
+temp_game_status_for_debugging <- game_status[1 != 0 , .(GAME_SLOT_ID,
+                                                         PIECE_ATTRIBUTE,
+                                                         START,
+                                                         FINISH,
+                                                         EXTRA,
+                                                         LANE_NO,
+                                                         SQUARE_ID,
+                                                         CYCLER_ID)]
+
+#temp_game_status_for_debugging <- set_cycler_position(cycler_id, 10, 1, temp_game_status_for_debugging)
+
+cycler_pos <- temp_game_status_for_debugging[CYCLER_ID == cycler_id, GAME_SLOT_ID]
+
+
+aggr_to_slots <- temp_game_status_for_debugging[is.na(EXTRA) &
+                                                # GAME_SLOT_ID <= game_status[FINISH == 1, GAME_SLOT_ID] #&
+                                                  GAME_SLOT_ID >= cycler_pos
+                                                , .N, by = .(GAME_SLOT_ID, PIECE_ATTRIBUTE, FINISH)]
 
 
 aggr_to_slots[, mountain_row := ifelse(PIECE_ATTRIBUTE == "M", 1, 0)]
@@ -17,30 +38,6 @@ restricted_v <- aggr_to_slots[, start_of_restricted_movement] == 1
 ascend_v <- aggr_to_slots[, PIECE_ATTRIBUTE == "A"]
 
 #function cost from slot A to B
-slot_A <- 15
-slot_B <- 18
-cost_AB <- function(slot_A, slot_B, slot_A_attr, slot_A_restr) {
-
-  #if start is A and distance <= 4, then impossible,
-  #if start ia A and distance == 5, then cost is 2
-  #if start is A and distance >= 6, then cost is distance
-
-  calc_distance <- slot_B - slot_A
-  result <- calc_distance
-  if (calc_distance <= 4 & slot_A_attr == "A") {
-    result <- 100000000
-  } else if (slot_A_attr == "A" & calc_distance == 5) {
-    result <- 2
-  }
-  #then resrict
-
-  if (slot_A_restr == 1 & calc_distance > 5) {
-    result <- 10000000
-  }
-  result <- as.integer(result)
-return(result)
-}
-#then create cost matrix
 
 landing_slots <- aggr_to_slots[, .(landing_slot = GAME_SLOT_ID, key = "1")]
 
@@ -57,64 +54,15 @@ rivi_lkm <- filter_nine_or_less[, uniqueN(GAME_SLOT_ID)]
 sarake_lkm <- filter_nine_or_less[, uniqueN(landing_slot)]
 
 cost_matrix <- matrix(data = filter_nine_or_less[, cost], nrow = rivi_lkm, ncol = sarake_lkm, byrow = FALSE)
-str(cost_matrix)
-cost_matrix[5, 7]
-
-is_legal <- function(input_data, i, j) {
-
- # tot_res <- as.vector(x = rep(TRUE, length(i) * length(j)))
- tot_res <- NULL
-    countteri <- 0
- datat <- data.table(iloop = i, jloop = j)
- looppikierrokset <- nrow(datat)
-
-   for (loopperi in 1:looppikierrokset) {
-     countteri <- countteri + 1
-     tot_res[countteri] <- input_data[GAME_SLOT_ID == datat[loopperi, iloop] & landing_slot == datat[loopperi, jloop], legal_move] == 0
-   }
-
-  # ret_matrix <- matrix(tot_res, nrow = length(i), ncol = length(j))
-       return(tot_res)
-}
-  # if (ressi == 1) {
-  #   return(TRUE)
-  # } else {
-  #   return(FALSE)
-  # }
 
 
 
-#install.packages("ompr")
-#
-#install.packages("ompr.roi")
-#install.packages()
+cyc_dec <- deck_status[CYCLER_ID == cycler_id]
 
-
-#etäisyysmatriisi
-
-game_status
-
-
-
-#install.packages("ROI.plugin.glpk")
-library(dplyr)
-library(ROI)
-library(ROI.plugin.glpk)
-
-library(ROI.plugin.symphony)
-library(ompr)
-library(ompr.roi)
-
-omaFiter <- function(ainat, test) {
-  res <- c(1, 1, 1)
-  return(res)
-}
-
-input_cycler_id <- 6
-cyc_dec <- deck_status[CYCLER_ID == input_cycler_id]
-
-kortit_Dt <-  rbind(data.table(CYCLER_ID = input_cycler_id, CARD_ID = 2, Zone = "Deck", MOVEMENT = 2, row_id = 1:3), cyc_dec)
-kortit <- kortit_Dt[, MOVEMENT]
+kortit_Dt <-  rbind(data.table(CYCLER_ID = input_cycler_id, CARD_ID = 2, Zone = "Deck", MOVEMENT = 2, row_id = 1:10), cyc_dec)
+kortit_aggr <- kortit_Dt[, .(cards_in_hand = .N), by = MOVEMENT]
+kortit <- kortit_aggr[, MOVEMENT]
+card_count <- kortit_aggr[, cards_in_hand]
 
 boundi_filtteri <- function(i, j, k, restricted_v, ascend_v) {
   rividata <- data.table(i, j, k, restricted_v, ascend_v)
@@ -124,32 +72,31 @@ boundi_filtteri <- function(i, j, k, restricted_v, ascend_v) {
   rividata[ascend_v == TRUE & distance == 5, tulos_save := !(k >= 2 & k <= 5)] #caset, missä pääsee laskeen alamäkeen
   rividata[ascend_v == TRUE & distance < 5, tulos_save := TRUE] #mahdoton lasketella alle 5
   rividata[ascend_v == TRUE & distance > 5, tulos_save := !(k == distance)] #voi mennä kovempaa ku 5 alamäessä
-  rividata[restricted_v == TRUE, tulos_save := !(distance <= 5 & k >= distance & distance >=  2)] #ei voi mennä yli 5 ylämäessä
 
 #  rividata[, tulos_save := !(((j - i) <= k)  & ((j - i) >= 2))] #either of these cases is true, then it is filterd out
   return(rividata[, tulos_save])
 }
-kortit
+
 
 model <- MILPModel() %>%
   #  add_variable(y[k], k = 1:20, type = "binary") %>%
   add_variable(x[i, j, k], i = 1:rivi_lkm, j = 1:rivi_lkm, k = 1:length(kortit), type = "binary") %>%
   #set_objective(sum_expr(y[k], k = 1), "max") %>%
   set_objective(sum_expr(x[i, j, k], i = 1:rivi_lkm, j = 1:rivi_lkm, k = 1:length(kortit)), "min") %>%
-  set_bounds(x[i, j, k], i = 1:rivi_lkm, j = 1:rivi_lkm, k = 1:length(kortit), ub = 0, boundi_filtteri(i, j, kortit[k], restricted_v, ascend_v))
-%>%
+  set_bounds(x[i, j, k], i = 1:rivi_lkm, j = 1:rivi_lkm, k = 1:length(kortit), ub = 0, boundi_filtteri(i, j, kortit[k], restricted_v, ascend_v)) %>%
+ add_constraint(sum_expr(x[i, j, k], i = 1:rivi_lkm, j = 1:rivi_lkm) <= card_count[k], k = 1:length(kortit)) # %>%
   #add_constraint(ruudut[i, j] * x[i, j, k] <= y[k] * kortit[k], j = 1:rivi_lkm, i = 1:rivi_lkm , k = 1:20) %>%
   #tätä ei tarvi, koska se on jo boundeissa
   #model %>% add_constraint(colwise(ruudut[i, j] * x[i, j, k] <=  kortit[k], j = 1:rivi_lkm, i = 1:rivi_lkm , k = 1:length(kortit)) #%>%
 
   #käytä kortti vain kerran
-  res <- model %>%  add_constraint(sum_expr(x[i, j, k], i = 1:rivi_lkm, j = 1:rivi_lkm) <= 1, k = 1:length(kortit)) %>%
+  res <- model %>% # add_constraint(sum_expr(x[i, j, k], i = 1:rivi_lkm, j = 1:rivi_lkm) <= 1, k = 1:length(kortit)) %>%
   #eka sarake alotettava
    add_constraint(sum_expr(x[i, j, k], j = 1:rivi_lkm, k = 1:length(kortit)) == 1, i = 1:1) %>%
   #ruudussa voi käydä vaan kerran
   add_constraint(sum_expr(x[i, j, k], j = 1:rivi_lkm, k = 1:length(kortit)) <= 1, i = 1:rivi_lkm)  %>%
   #jatka siitä mihin jäit
-  add_constraint(sum_expr(x[n, j, k], j = 1:rivi_lkm, k = 1:length(kortit)) == sum_expr(x[i, n,  k], i = 1:rivi_lkm, k = 1:length(kortit)), n = 2:83) %>%
+  add_constraint(sum_expr(x[n, j, k], j = 1:rivi_lkm, k = 1:length(kortit)) == sum_expr(x[i, n,  k], i = 1:rivi_lkm, k = 1:length(kortit)), n = 2:(rivi_lkm - 1)) %>%
   # add_constraint(x[i, j, k]  <= sum_expr(x[i + kortit[k], j, k], i = 1:7, j = 1:length(kortit), k = 1:5) , i = 1:10, j = 1:10, k = 1:5) %>%# )
   #maaliin on päästävä
   add_constraint(sum_expr(x[i, j, k], i = 1:rivi_lkm, k = 1:length(kortit)) == 1, j = rivi_lkm) %>%
